5bdb8500f4ea745e8efbe950fafb33b9a2aebcbb
André Gonçalves <andreslb1@gmail.com>
2015-07-29 10:39:04 +0200

    Update for live

--- collective/object/utils/vocabularies.py
+++ collective/object/utils/vocabularies.py
@@ -216,11 +216,11 @@ class ATVMVocabulary(object):
         units = atvm.getVocabularyByName(self.name)
         terms = []
 
-        if self.name == "TaxonomyRank":
-            taxonomies = ["kingdom", "subkingdom", "phylum or division", "subphylum or subdivision", "superclass", "class", "subclass", "infraclass", "superorder", "order", "suborder", "infraorder", "superfamily", "family", "subfamily", "tribe", "subtribe", "genus", "subgenus", "section", "subsection", "species", "subspecies", "variety", "subvariety", "form", "subform"]
-            for taxonomy in taxonomies:
-                if taxonomy not in units:
-                    atvm.TaxonomyRank.addTerm(taxonomy.encode('ascii', 'ignore'), str(taxonomy))
+        #if self.name == "TaxonomyRank":
+        #    taxonomies = ["kingdom", "subkingdom", "phylum or division", "subphylum or subdivision", "superclass", "class", "subclass", "infraclass", "superorder", "order", "suborder", "infraorder", "superfamily", "family", "subfamily", "tribe", "subtribe", "genus", "subgenus", "section", "subsection", "species", "subspecies", "variety", "subvariety", "form", "subform"]
+        #    for taxonomy in taxonomies:
+        #        if taxonomy not in units:
+        #            atvm.TaxonomyRank.addTerm(taxonomy.encode('ascii', 'ignore'), str(taxonomy))
 
         for term in units:
             if units[term]:
