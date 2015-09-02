9ebc1abd4d565c21a59a303aac9dd08189549041
André Gonçalves <andreslb1@gmail.com>
2015-09-02 13:08:10 +0200

    Separate object view for logged in users

--- collective/object/utils/views.py
+++ collective/object/utils/views.py
@@ -6,7 +6,13 @@ from zope.component import getMultiAdapter
 from Products.CMFCore.utils import getToolByName
 from collective.object import MessageFactory as _
 from plone.dexterity.browser.view import DefaultView
-
+from AccessControl import getSecurityManager
+from Products.CMFCore.permissions import ModifyPortalContent
+from zope.schema.interfaces import IChoice, ITextLine, IList, IText, IBool
+from collective.z3cform.datagridfield.interfaces import IDataGridField
+from plone.app.textfield.interfaces import IRichText
+from collective.object.utils.interfaces import IListField
+from z3c.relationfield.interfaces import IRelationList
 
 # # # # # # # # # # # # #
 # View specific methods #
@@ -14,6 +20,139 @@ from plone.dexterity.browser.view import DefaultView
 
 class ObjectView(DefaultView):
     """ View class """
+
+    """
+    MultiContentTreeWidget
+    TextWidget
+    BlockDataGridField
+    DataGridField
+    TextAreaWidget
+    SelectWidget
+
+    """
+    def get_fieldtype_by_schema(self, field):
+        f = str(field)
+
+        type_field = ""
+        if "MultiContentTreeWidget" in f:
+            type_field = "relation"
+        elif "TextWidget" in f:
+            type_field = "text"
+        elif "BlockDataGridField" in f:
+            type_field = "datagrid"
+        elif "DataGridField" in f:
+            type_field = "datagrid"
+        elif "TextAreaWidget" in f:
+            type_field = "text"
+        elif "SelectWidget" in f:
+            type_field = "select"
+        else:
+            type_field = "unknown"
+
+        return type_field
+
+    def checkUserPermission(self):
+        sm = getSecurityManager()
+        if sm.checkPermission(ModifyPortalContent, self.context):
+            return True
+        return False
+
+    def transform_value(self, value):
+        if value == "selected":
+            value = "Ja"
+
+        if value == False:
+            value = "Nee"
+
+        if value == True:
+            value = "Ja"
+
+        return value
+
+    def show_fieldset(self, fieldset):
+        for widget in fieldset.widgets.values():
+            if widget.value:
+                return True
+        return False
+
+    def get_url_by_uid(self, uid):
+        catalog = self.context.portal_catalog
+        brains = catalog(UID=uid)
+        if brains:
+            obj = brains[0]
+            return obj.getURL()
+
+        return ""
+
+    def append_value(self, _list, value):
+      
+        if 'collective.exhibition.exhibition.Exhibition' in str(type(value)):
+            uid = value.UID()
+
+            url = self.get_url_by_uid(uid)
+            value = "<a href='%s'>%s</a>" %(url, value.title)
+            _list.append(value)
+            return
+
+        if value != "" and value != None and value != " " and value != []:
+            _list.append(value)
+
+    def generate_value_from_item(self, item, line):
+        if type(item) is str:
+            self.append_value(line, self.transform_value(item))
+        
+        elif type(item) is list:
+            for elem in item:
+                if type(elem) is dict:
+                    for key in elem.keys():
+                        #val = "%s: %s<p>" %(key, elem[key])
+                        val = elem[key]
+                        self.append_value(line, self.transform_value(val))
+                
+                elif type(elem) is list:
+                    for e in elem:
+                        self.append_value(line, self.transform_value(e))
+                else:
+                    self.append_value(line, self.transform_value(elem))
+                
+                self.append_value(line, '<p>')
+        else:
+            for key in item.keys():
+                if type(item[key]) is list:
+                    for val in item[key]:
+                        self.append_value(line, self.transform_value(val))
+                else:
+                    #val = "%s: %s<p>" %(key, item[key])
+                    val = item[key]
+                    self.append_value(line, self.transform_value(val))
+
+    def get_field_value(self, value, widget):
+        _type = self.get_fieldtype_by_schema(widget)
+
+        if value == None:
+            return ""
+
+        if _type in ["datagrid", "select"]:
+            try:
+                result = []
+                for item in value:
+                    line = []
+                    self.generate_value_from_item(item, line)
+                    #print line
+                    line = ', '.join(line)
+                    
+                    result.append(line)
+                result = '<p>'.join(result)
+                return result
+            except:
+                raise
+                return ""
+
+        elif _type == "relation":
+            return ""
+
+
+        return value
 
     def trim_white_spaces(self, text):
         if text != "" and text != None:
