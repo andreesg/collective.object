56c21e25701a8ea7a88612597ba03287b06c29db
André Gonçalves <andreslb1@gmail.com>
2015-09-09 17:13:54 +0200

    Back to regular related items widget

--- collective/object/utils/source.py
+++ collective/object/utils/source.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from plone.formwidget.contenttree.source import PathSourceBinder, PathSource
+from plone.formwidget.contenttree.source import PathSourceBinder, PathSource, ObjPathSource
 from zope.schema.vocabulary import SimpleTerm
 
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # Extended function that generates SimpleTerm for each related item   #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-class ObjPathSource(PathSource):
+
+
+
+class ObjExtendedPathSource(ObjPathSource):
 
     def _getBrainByValue(self, value):
         try:
@@ -15,7 +18,7 @@ class ObjPathSource(PathSource):
                 if not value.absolute_url():
                     portal_catalog = self.context.portal_catalog
                     brains = portal_catalog(UID=value.UID())
-                    if len(brains) > 0:
+                    if brains:
                         brain = brains[0]
                         return brain
 
@@ -23,37 +26,10 @@ class ObjPathSource(PathSource):
         except:
             pass
 
-    def getTermByBrain(self, brain, real_value=True):
-    	item = brain.getObject()
-
-        if real_value:
-            value = brain._unrestrictedGetObject()
-        else:
-            value = brain.getPath()[len(self.portal_path):]
-
-        title = brain.Title
-        if hasattr(item, 'identification_identification_objectNumber'):
-            title = "%s - %s" %(item.identification_identification_objectNumber, title)
-
-        if hasattr(item, 'titleAuthorImprintCollation_titleAuthor_author'):
-            authors = item.titleAuthorImprintCollation_titleAuthor_author
-            if authors:
-                author_name = authors[0]['author']
-                if author_name:
-                    title = "%s - %s" %(author_name, title)
-
-        url = brain.getURL()
-
-        # Adds link to access related item
-        title = '<a href="%s">%s</a>' %(url, title)
-
-        return SimpleTerm(value, token=brain.getPath(), title=title or
-                          brain.id)
-
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # Extended function that uses custom ObjPathSource class    #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 class ObjPathSourceBinder(PathSourceBinder):
-    path_source = ObjPathSource
+    path_source = ObjExtendedPathSource
 
 
