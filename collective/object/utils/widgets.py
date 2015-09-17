4f9a5cf5f66d1eb6d9fbfdf53c0f9ed81ec1072a
André Gonçalves <andreslb1@gmail.com>
2015-09-17 18:15:05 +0200

    hotfix

--- collective/object/utils/widgets.py
+++ collective/object/utils/widgets.py
@@ -28,7 +28,50 @@ class AjaxSingleSelectWidget(AjaxSelectWidget):
         """
 
         args = super(AjaxSingleSelectWidget, self)._base_args()
+
+        args['name'] = self.name
+        args['value'] = self.value
+
+        args.setdefault('pattern_options', {})
+
+        field_name = self.field and self.field.__name__ or None
+
+        context = self.context
+        # We need special handling for AddForms
+        if IAddForm.providedBy(getattr(self, 'form')):
+            context = self.form
+
+        vocabulary_name = self.vocabulary
+        field = None
+
         args['pattern_options']['maximumSelectionSize'] = 1
+        field = self.field.value_type
+        
+        #if IChoice.providedBy(self.field):
+        #    args['pattern_options']['maximumSelectionSize'] = 1
+        #    field = self.field
+        #elif ICollection.providedBy(self.field):
+        #    field = self.field.value_type
+
+
+        if not vocabulary_name and field is not None:
+            vocabulary_name = field.vocabularyName
+
+        args['pattern_options'] = dict_merge(
+            get_ajaxselect_options(context, args['value'], self.separator,
+                                   vocabulary_name, self.vocabulary_view,
+                                   field_name),
+            args['pattern_options'])
+
+        if field and getattr(field, 'vocabulary', None):
+            form_url = self.request.getURL()
+            source_url = "%s/++widget++%s/@@getSource" % (form_url, self.name)
+            args['pattern_options']['vocabularyUrl'] = source_url
+
+        # ISequence represents an orderable collection
+        if ISequence.providedBy(self.field) or self.orderable:
+            args['pattern_options']['orderable'] = True
+
         return args
 
 class SimpleRelatedItemsWidget(RelatedItemsWidget):
